972e0f7f5a1879f30be2256fcabca4a60d5cbe31
xueyingwang <xueyingwang2611@gmail.com>
2025-03-19 16:28:03 +0000

    update3.19

--- srcweb/urls.py
+++ srcweb/urls.py
@@ -5,7 +5,8 @@ app_name = 'srcweb'
 urlpatterns = [
     path('', views.home, name='home'),
     path('events/', views.events, name='events'),
-    path('eventdetails/', views.eventdetails, name='eventdetails'),
+    path('eventdetails_student/', views.eventdetails_student, name='eventdetails_student'),
+    path('eventdetails_staff/', views.eventdetails_staff, name='eventdetails_staff'),
     path('myaccount/', views.myaccount, name='myaccount'),
     path('dancingsociety/', views.dancingsociety, name='dancingsociety'),
 
--- srcweb/views.py
+++ srcweb/views.py
@@ -7,11 +7,15 @@ def home(request):
 def events(request):
     return render(request, 'srcweb/events.html')
 
-def eventdetails(request):
-    return render(request, 'srcweb/eventdetails.html')
+def eventdetails_student(request):
+    return render(request, 'srcweb/eventdetails_student.html')
+def eventdetails_staff(request):
+    return render(request, 'srcweb/eventdetails_staff.html')
 
 def myaccount(request):
     return render(request, 'srcweb/myaccount.html')
 
 def dancingsociety(request):
     return render(request, 'srcweb/dancingsociety.html')
+
+
